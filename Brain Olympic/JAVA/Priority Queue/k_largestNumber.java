73d5496f4760a53b8fb7e71f780f9d97b00bd1e6
Gopal <gopalmittal2003@gmail.com>
2024-02-12 14:50:08 +0530

    Priorit_Q which  prints k largest elements

--- Brain Olympic/JAVA/Priority Queue/k_largestNumber.java
+++ Brain Olympic/JAVA/Priority Queue/k_largestNumber.java
@@ -4,6 +4,7 @@ public class k_largestNumber {
     public static void main(String[] args) {
         int k = 4;
         Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> pqO = new PriorityQueue<>();
         pq.add(1);
         pq.add(8);
         pq.add(9);
@@ -12,12 +13,13 @@ public class k_largestNumber {
         pq.add(5);
         pq.add(7);
 
-        while(k > 0 && !pq.isEmpty()) 
+        while(k > 0) 
         {
-            System.out.println(pq.remove());
-
+            pqO.add(pq.remove());
             k--;
-        }    
+        }  
+        
+        while(!pqO.isEmpty()) System.out.println(pqO.remove());
 
     }
 }
