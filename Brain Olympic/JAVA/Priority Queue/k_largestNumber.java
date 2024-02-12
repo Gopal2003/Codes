28780e85f806fbe9f895548f65d4b11fe8575fce
Gopal <gopalmittal2003@gmail.com>
2024-02-12 14:52:05 +0530

    Added a normal queue to remove k largest element in increasing order

--- Brain Olympic/JAVA/Priority Queue/k_largestNumber.java
+++ Brain Olympic/JAVA/Priority Queue/k_largestNumber.java
@@ -4,7 +4,7 @@ public class k_largestNumber {
     public static void main(String[] args) {
         int k = 4;
         Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> pqO = new PriorityQueue<>();
+        Queue<Integer> pqO = new PriorityQueue<>(); // normal queue
         pq.add(1);
         pq.add(8);
         pq.add(9);
