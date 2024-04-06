73cab429816bb67ec5fa825688719feffba09715
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-04-06 18:28:37 +0530

    Printing a matrix which shows the path travelled

--- Brain Olympic/Backtracking/maze.java
+++ Brain Olympic/Backtracking/maze.java
@@ -8,7 +8,8 @@ public class maze{
         {
             Arrays.fill(d,1);
         }
-        printMazePath("", arr, 0, 0);
+        int [][] path = new int[3][3];
+        printMazePath("", arr, 0, 0,path,1);
     }
 
     public static void print(int[][] arr)
@@ -23,39 +24,78 @@ public class maze{
         }
     }
 
-    public static void printMazePath(String s,int[][] arr,int r,int c)
+    // public static void printMazePath(String s,int[][] arr,int r,int c)
+    // {
+    //     if(r == arr.length - 1 && c == arr[0].length - 1)
+    //     {
+    //         arr[r][c] = 0;
+    //         print(arr);
+    //         System.out.println(s);
+    //         arr[r][c] = 1;
+    //         return;
+    //     }
+    //     if(arr[r][c] == 0) return;
+        
+    //     arr[r][c] = 0;
+        
+    //     if(r < arr.length - 1)
+    //     {
+    //         // System.out.println("IN");
+    //         printMazePath(s + 'D', arr, r + 1, c);
+    //     }
+    //     if(c < arr[0].length - 1)
+    //     {
+    //         printMazePath(s + 'R', arr, r, c + 1);
+    //     }
+    //     if(r > 1)
+    //     {
+    //         printMazePath(s + 'U', arr, r - 1, c);
+    //     }
+    //     if(c > 1)
+    //     {
+    //         printMazePath(s + 'L', arr, r, c - 1);
+    //     }
+
+    //     arr[r][c] = 1;
+        
+    // }
+
+    public static void printMazePath(String s,int[][] arr,int r,int c,int[][] path,int step)
     {
         if(r == arr.length - 1 && c == arr[0].length - 1)
         {
-            arr[r][c] = 0;
-            print(arr);
+            // System.out.println(path[r][c]);
+            path[r][c] = step;  
+            print(path);
             System.out.println(s);
-            arr[r][c] = 1;
+            // path[r][c] = 0;
             return;
         }
         if(arr[r][c] == 0) return;
         
         arr[r][c] = 0;
+        path[r][c] = step;
         
         if(r < arr.length - 1)
         {
             // System.out.println("IN");
-            printMazePath(s + 'D', arr, r + 1, c);
+            printMazePath(s + 'D', arr, r + 1, c,path,step + 1);
         }
         if(c < arr[0].length - 1)
         {
-            printMazePath(s + 'R', arr, r, c + 1);
+            printMazePath(s + 'R', arr, r, c + 1,path,step + 1);
         }
         if(r > 1)
         {
-            printMazePath(s + 'U', arr, r - 1, c);
+            printMazePath(s + 'U', arr, r - 1, c,path,step + 1);
         }
         if(c > 1)
         {
-            printMazePath(s + 'L', arr, r, c - 1);
+            printMazePath(s + 'L', arr, r, c - 1,path,step + 1);
         }
 
         arr[r][c] = 1;
+        path[r][c] = 0;
         
     }
 }
