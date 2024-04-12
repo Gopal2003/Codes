9b761fb9c1faf9cc1e925d6ebd34ba7d0fd433e3
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-04-12 15:59:16 +0530

    BalanceParanthesis

---
+++ Brain Olympic/LB & KK/Recursion KK Assignment JAVA/Xl_BalancedParanthesis.java
@@ -0,0 +1,50 @@
+public class Xl_BalancedParanthesis {
+    public static void main(String[] args)
+    {
+        String s = "[()](){()}";
+        String opens = "";
+        System.out.print(checkParanthesis(s, 0,opens));
+    }
+
+
+    public static boolean checkParanthesis(String s,int index,String openBrackets)
+    {
+        int lengthOfS = s.length(),lengthOfOpenBrackets = openBrackets.length();
+        if(index == lengthOfS)
+        {
+            if(lengthOfOpenBrackets == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        char ch = s.charAt(index);
+        if(ch == '(' || ch == '[' || ch == '{')
+        {
+            openBrackets = openBrackets + ch;
+        }
+        else
+        {
+            char lastChar = openBrackets.charAt(lengthOfOpenBrackets - 1);
+            if(ch == ')' && lastChar == '(')
+            {
+                openBrackets = openBrackets.substring(0,lengthOfOpenBrackets - 1 );
+            }
+            else if(ch == ']' && lastChar == '[')
+            {
+                openBrackets = openBrackets.substring(0,lengthOfOpenBrackets - 1);
+            }
+            else if(ch == '}' && lastChar == '{')
+            {
+               openBrackets = openBrackets.substring(0,lengthOfOpenBrackets - 1);
+            }
+        }
+        
+        return(checkParanthesis(s, index + 1,openBrackets));
+    }
+}
