8efe587280d374b81560cebdfb66e5c88d035d95
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-04-23 19:19:19 +0530

    New syntax of switch

---
+++ Brain Olympic/Switch/WeekdaysWeekends.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class WeekdaysWeekends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        switch (num) {
+            // case 1:
+            // case 2:
+            // case 3:
+            // case 4:
+            // case 5:
+            case 1,2,3,4,5:
+                System.out.println("Weekdays");
+                break;
+            // case 6:
+            // case 7:
+            case 6,7:
+                System.out.println("Weekends");
+                break;
+
+            default:
+                System.out.println("Enter the valid number");
+                break;
+            // break;
+
+        }
+
+        // switch(num){
+        //     case 1,2,3,4,5 -> System.out.println("Weekdays");
+        //     case 6,7 -> System.out.println("Weekends");
+        //     default -> System.out.println("Enter the valid number");
+        // }
+    }
+}
