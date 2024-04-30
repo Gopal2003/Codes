594ee3a2fcd584b15b5155867e2431620b3178a6
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-04-30 18:07:31 +0530

    Final keyword

---
+++ Object Oriented Programming  in JAVA/FInal/Human.java
@@ -0,0 +1,36 @@
+public final class Human {
+    static final int getNum(int n)
+    {
+        if(n > 10)
+        {
+             return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    static final int getNum(int a,int b)
+    {
+        if(a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+
+    int getNum(String s)
+    {
+        return 1;
+    }
+
+    Human()
+    {
+        System.out.println("HEHE");
+    }
+
+}
---
+++ Object Oriented Programming  in JAVA/FInal/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        Human b1 = new Human();
+        System.out.println(Human.getNum(11));
+        System.out.println(Human.getNum(11,12));
+        System.out.println(b1.getNum(3,4));
+    }
+} 
