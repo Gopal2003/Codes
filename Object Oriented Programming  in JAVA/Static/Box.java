ac79d51210228678ac14436e6efa5c3705bba4fc
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-04-30 18:15:52 +0530

    Static Keyword

---
+++ Object Oriented Programming  in JAVA/Static/Box.java
@@ -0,0 +1,8 @@
+public class Box{
+
+    static void greetings() 
+    {
+        System.out.println("Hey, I am in Box class. Greetings!");
+    }
+
+}
---
+++ Object Oriented Programming  in JAVA/Static/BoxWeight.java
@@ -0,0 +1,8 @@
+public class BoxWeight extends Box {
+    
+    // @Override
+    // void greetings() //! Pop error due to static
+    // {
+    //     System.out.println("Hey, I am in BoxWeight Class. Greetings!");
+    // }
+}
