835b2447854b254742146cafe52e53334c59d6f3
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-05-03 18:30:14 +0530

    GetterSetter

---
+++ Object Oriented Programming  in JAVA/Access Control/GetterSetter.java
@@ -0,0 +1,18 @@
+public class GetterSetter{
+    private int num;
+    String name;
+
+    // GetterSetter(int num)
+    // {
+    //     this.num = num;
+    // }
+    public int getter()
+    {
+        return num;
+    }
+
+    public void setter(int num)
+    {
+        this.num = num;
+    }
+}
---
+++ Object Oriented Programming  in JAVA/Access Control/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // GetterSetter obj1 = new GetterSetter(7);
+        GetterSetter obj1 = new GetterSetter();
+        obj1.setter(9);
+       System.out.println(obj1.getter());
+
+    }
+}
