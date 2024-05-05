4da71aa3d8dbba9dd071822a7e107368166ba185
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-05-05 11:15:08 +0530

    Operators in strings

---
+++ JAVA Basics/Strings/Operators.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // !Adds the ASCII values of both the characters
+        System.out.println("a" + "b");// !Concatinates
+        System.out.println((char)('a' + 3));
+
+        System.out.println("a" + 1);
+        //! integer will be converted to Integer that will call toString().
+
+        System.out.println("a" + 'a');
+
+        System.out.println("Gopal" + new ArrayList<>());
+
+       ArrayList<Integer> n = new ArrayList<>();
+
+       n.add(4);
+       System.out.println(n);
+        // System.out.println(Arrays.toString(arr));
+
+        // Integer a = 5;
+        // Integer b = 5;
+        // System.out.println(a == b); // true
+
+        // Integer a = new Integer(5);
+        // Integer b = new Integer(5);
+        // System.out.println(a == b);//false
+    }
+}
---
+++ JAVA Basics/Strings/PrettyPrinting.java
@@ -0,0 +1,13 @@
+public class PrettyPrinting{
+    public static void main(String[] args) {
+    //    float n = 4346.34534f;
+    //    System.out.printf("Formatted number is %.2f",n);// Rounds Off the number as well...
+
+        //   System.out.println(Math.PI);
+        //   System.out.printf("Formated Pi %.2f",Math.PI);
+
+        System.out.printf("Hi my name is %s and I am %s boy","Gopal","good"); //! %s is a placeholder which is pre-defined. Hence, if we use another placeholder, it will not work!!.
+
+
+    }
+}
