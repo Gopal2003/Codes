da266b739c7d50378d041e0ad66d2674bad57825
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-05-20 21:59:15 +0530

    Resources

---
+++ Object Oriented Programming  in JAVA/Exception Handling/Execp1.java
@@ -0,0 +1,36 @@
+public class Execp1{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        try{
+            int c = divide(a, b);
+            throw new Exception("Just for fun"); // Example to execute the Exeception e catch block.
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        // catch(ArithmeticException e) //! Error- Unreachable catch block for ArithmeticException. It is already handled by the catch block for ArithmeticException
+        // {
+        //     System.out.println(e.getMessage());
+        // }
+        finally{
+            System.out.println("This will always execute");
+        }
+    }
+
+   static int divide(int a,int b) throws ArithmeticException //! It is used for declaration. It tells that hey, this function may throws exeception.
+   {
+        if (b == 0) {
+            throw new ArithmeticException("Please Don't divide with zero"); //! Used to actually throw exeception.
+        }
+
+        return a/b;
+        
+    }
+   
+}
---
+++ Object Oriented Programming  in JAVA/Exception Handling/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Execp1.divide(1, 0);
+    }
+}
