889661650b98712ed54ab8563e003d6b6f3836f5
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-05-24 18:18:11 +0530

    Collections

--- Object Oriented Programming  in JAVA/Inheritence/Multiple Inheritence/Derived.java
+++ Object Oriented Programming  in JAVA/Inheritence/Multiple Inheritence/Derived.java
@@ -1,7 +1,3 @@
-class Derived extends Base1, Base2 {
-    // You can access n from each base class explicitly
-    void display() {
-        System.out.println("n from Base1: " + super.n);
-        System.out.println("n from Base2: " + super.n);
-    }
+public class Derived{
+    int money = 345634;
 }
---
+++ Object Oriented Programming  in JAVA/Inheritence/Multiple Inheritence/In.java
@@ -0,0 +1,3 @@
+public class In {
+    int money = 534645;
+}
