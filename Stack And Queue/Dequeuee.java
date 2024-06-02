ac99d6516d5db9b8eee6fc4027f6bfeb67affe0a
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-02 12:11:50 +0530

    Stack and Queues

---
+++ Stack And Queue/Dequeuee.java
@@ -0,0 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.*;
+
+public class Dequeuee {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new ArrayDeque<>();
+    }
+}
---
+++ Stack And Queue/Queuee.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queuee {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        System.out.println(q.peek());
+    }
+}
