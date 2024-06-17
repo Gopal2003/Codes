c3329ed648b59ea024ff6e29fed34d2ae6959d81
Gopal Agarwal <gopalmittal2003@gmail.com>
2024-06-17 11:36:30 +0530

    Segment Tree Base code

---
+++ Trees/Segment Trees/SegmentTrees.java
@@ -0,0 +1,56 @@
+public class SegmentTrees{
+
+    public class Node{
+        int data;
+        int startInterval,endInterval;
+        Node left;
+        Node right;
+
+        Node(int data)
+        {
+            this.data = data;
+        }
+
+        Node(int startInterval,int endInterval)
+        {
+            this.startInterval = startInterval;
+            this.endInterval = endInterval;
+        }
+    }
+
+    private Node root;
+
+    public void constructTree(int[] arr)
+    {
+        int length = arr.length;
+        root = constructTree(arr,0,length - 1);
+    }
+
+    public Node constructTree(int[] arr,int start,int end)
+    {
+        //Base case where the start and end pointer pointing to the same index. Hence, create a new node with the data from the array and return it.
+        if(start == end)
+        {
+            Node leaf = new Node(start,end);
+            leaf.data = arr[start];
+            return leaf;
+        }
+
+        //Create a Empty node with start and end range in it.
+        Node node = new Node(start, end);
+
+        int mid = start + (end - start)/2;
+
+        //Get the left and right node so that using these nodes we can calculate the current node's value.Basically following the bottom-up approach 
+        //where we fill the bottom nodes first and then the parent nodes.
+        node.left = constructTree(arr, start, mid);
+        node.right = constructTree(arr, mid + 1, end);
+
+        node.data = node.left.data + node.right.data;
+
+        return node;
+        
+    }
+
+
+}
