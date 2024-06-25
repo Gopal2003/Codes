0fb4524d38d459cded12fa018a939e8d548d0afd
Gopal <gopalmittal2003@gmail.com>
2024-06-25 17:05:34 +0530

    BufferReader

--- File Handling in Java/InputStream.java
+++ File Handling in Java/InputStream.java
@@ -1,8 +1,11 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class InputStream {
     public static void main(String[] args) {
+        
         try(InputStreamReader Isr = new InputStreamReader(System.in))
         {
             System.out.println("Enter some letters");
@@ -12,6 +15,17 @@ public class InputStream {
                 System.out.println((char)letters);
                 letters = Isr.read();
             }
+            Isr.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        
+
+        try(BufferedReader bR = new BufferedReader(new InputStreamReader(System.in)))
+        {
+            System.out.println("You Typed: " + bR.readLine());
         }
         catch(IOException e)
         {
