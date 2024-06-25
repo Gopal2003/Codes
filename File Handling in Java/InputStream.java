a62837bb87c02a5ed6cabdbe03fe1c9f5957e4a2
Gopal <gopalmittal2003@gmail.com>
2024-06-25 16:20:13 +0530

    InputStreamReader

---
+++ File Handling in Java/InputStream.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputStream {
+    public static void main(String[] args) {
+        try(InputStreamReader Isr = new InputStreamReader(System.in))
+        {
+            System.out.println("Enter some letters");
+            int letters = Isr.read();
+            while(Isr.ready())
+            {
+                System.out.println((char)letters);
+                letters = Isr.read();
+            }
+        }
+        catch(IOException e)
+        {
+
+        }
+    }
+}
