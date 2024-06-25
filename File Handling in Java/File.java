b341397632beb36525b784d0aaf5bfc8a9f3ab8a
Gopal <gopalmittal2003@gmail.com>
2024-06-25 16:14:16 +0530

    File Handling

--- File Handling in Java/File.java
+++ File Handling in Java/File.java
@@ -1,4 +1,5 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class File
@@ -19,6 +20,23 @@ public class File
         {
             System.out.println(e.getMessage());
         }
+
+        try(FileWriter fr = new FileWriter("newFile.txt"))
+        {
+            fr.append("Gopal Agarwal");
+            
+            fr.append('a');
+            fr.append('p');
+            fr.append('p');
+            fr.append('l');
+            fr.append('e');
+
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
