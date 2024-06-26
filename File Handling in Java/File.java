ad83281c06d781d69c896b84ab3f790251dbd0e6
Gopal <gopalmittal2003@gmail.com>
2024-06-26 14:58:33 +0530

    File Handling

--- File Handling in Java/File.java
+++ File Handling in Java/File.java
@@ -6,30 +6,32 @@ public class File
 {
     public static void main(String[] args)
     {
-        try(FileReader fr = new FileReader("File.txt"))
-        {
-            int letters = fr.read();
-            while(fr.ready())
-            {
-                System.out.println((char)letters);
-                letters = fr.read();
-            }
-            System.out.println();
-        }
-        catch(IOException e)
-        {
-            System.out.println(e.getMessage());
-        }
+        // try(FileReader fr = new FileReader("File.txt"))
+        // {
+        //     int letters = fr.read();
+        //     while(fr.ready())
+        //     {
+        //         System.out.println((char)letters);
+        //         letters = fr.read();
+        //     }
+        //     System.out.println();
+        // }
+        // catch(IOException e)
+        // {
+        //     System.out.println(e.getMessage());
+        // }
 
         try(FileWriter fr = new FileWriter("newFile.txt"))
         {
             fr.append("Gopal Agarwal");
-            
-            fr.append('a');
-            fr.append('p');
-            fr.append('p');
-            fr.append('l');
-            fr.append('e');
+
+            // fr.append('a');
+            // fr.append('p');
+            // fr.append('p');
+            // fr.append('l');
+            // fr.append('e');
+
+            fr.write("This is test");
 
         }
         catch(IOException e)
